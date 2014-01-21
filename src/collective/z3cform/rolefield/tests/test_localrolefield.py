9fff1e33deafe1919903e018ac86bda85157a83d
Jean-François Roche <jfroche@affinitic.be>
2014-01-21 16:11:17 +0100

    use fieldeventupdated event to apply local role changes

--- src/collective/z3cform/rolefield/tests/test_localrolefield.py
+++ src/collective/z3cform/rolefield/tests/test_localrolefield.py
@@ -2,14 +2,20 @@
 
 import unittest2 as unittest
 
+from zope import component
+from zope import schema
 from zope.interface import Invalid
 
-from plone.app.testing.helpers import setRoles
+from plone.app.testing import login, TEST_USER_NAME, setRoles, TEST_USER_ID
 
 from ecreall.helpers.testing.base import BaseTest
 
+from .container import ITestContainer
 from collective.z3cform.rolefield.testing import ROLEFIELD_PROFILE_FUNCTIONAL
-from collective.z3cform.rolefield.localrolefield import LocalRolesToPrincipalsDataManager
+from collective.z3cform.rolefield.localrolefield import (LocalRolesToPrincipals,
+                                                         remove_local_roles_from_principals,
+                                                         add_local_roles_to_principals)
+from ..interfaces import ILocalRolesToPrincipals
 
 
 class TestRoleField(unittest.TestCase, BaseTest):
@@ -20,9 +26,10 @@ class TestRoleField(unittest.TestCase, BaseTest):
     def setUp(self):
         super(TestRoleField, self).setUp()
         self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
 
     def _getTargetClass(self):
-        from collective.z3cform.rolefield.localrolefield import LocalRolesToPrincipals
         return LocalRolesToPrincipals
 
     def _makeOne(self, *args, **kw):
@@ -43,10 +50,67 @@ class TestRoleField(unittest.TestCase, BaseTest):
         field = self._makeOne(roles_to_assign=('Editor', 'Reader'))
         field.validate([])
 
-    def test_datamanager(self):
+    def test_events(self):
+        logs = []
+
+        @component.adapter(ITestContainer,
+                           ILocalRolesToPrincipals,
+                           schema.interfaces.IFieldUpdatedEvent)
+        def add_field_events(obj, field, event):
+            logs.append((event, obj, field))
+
+        component.provideHandler(add_field_events)
+        self.portal.invokeFactory('testingtype', 'test')
+        item = getattr(self.portal, 'test')
+        self.assertEqual(logs, [])
+        item.testingField = ['foo']
+        self.assertEqual(len(logs), 1)
+        event, obj, field = logs[0]
+        self.assertEqual(obj, item)
+        self.assertTrue(isinstance(field, LocalRolesToPrincipals))
+        self.assertEqual(event.old_value, schema.NO_VALUE)
+        self.assertEqual(event.new_value, ['foo'])
+
+    def test_remove_local_roles_from_principals(self):
+        self.portal.invokeFactory('testingtype', 'test')
+        item = getattr(self.portal, 'test')
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner',)})
+        remove_local_roles_from_principals(item, [TEST_USER_ID], ('Owner',))
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {})
+
+    def test_add_local_roles_to_principals(self):
+        self.portal.invokeFactory('testingtype', 'test')
+        item = getattr(self.portal, 'test')
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner',)})
+        add_local_roles_to_principals(item, [TEST_USER_ID], ('Editor',))
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner', 'Editor')})
+
+    def test_add_unknown_local_roles_to_principals(self):
+        self.portal.invokeFactory('testingtype', 'test')
+        item = getattr(self.portal, 'test')
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner',)})
+        add_local_roles_to_principals(item, [TEST_USER_ID], ('Coach',))
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner', 'Coach')})
+
+    def test_add_local_roles_to_unkown_principals(self):
+        self.portal.invokeFactory('testingtype', 'test')
+        item = getattr(self.portal, 'test')
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner',)})
+        add_local_roles_to_principals(item, ['John'], ('Editor',))
+        local_roles = dict(item.get_local_roles())
+        self.assertEqual(local_roles, {TEST_USER_ID: ('Owner',)})
+
+    def test_local_roles_assignation(self):
         """Test the local_roles assignment mechanism managed by the datamanager."""
         testingfield = self.portal.portal_types.testingtype.lookupSchema()['testingField']
-        testingfield.roles_to_assign = ('Editor', 'Contributor')
+        self.assertEqual(testingfield.roles_to_assign, ('Reader', 'Owner'))
         # first create a sample object
         # make the default user a Manager
         member = self.portal.portal_membership.getAuthenticatedMember()
@@ -54,33 +118,36 @@ class TestRoleField(unittest.TestCase, BaseTest):
         # create an object
         self.portal.invokeFactory('testingtype', id='testingobj')
         testingobj = getattr(self.portal, 'testingobj')
-        datamanager = LocalRolesToPrincipalsDataManager(testingobj, testingfield)
         self.failIf('Administrators' in testingobj.__ac_local_roles__.keys())
-        datamanager.set(('Administrators', ))
+        testingobj.testingField = ['Administrators', ]
         # now we have local_roles for 'Administrators'
         self.failUnless('Administrators' in testingobj.__ac_local_roles__.keys())
         # moreover, local_roles for 'Administrators' are ('Editor', 'Contributor')
-        self.assertEquals(tuple(testingobj.__ac_local_roles__['Administrators']), testingfield.roles_to_assign)
+        self.assertEquals(tuple(testingobj.__ac_local_roles__['Administrators']),
+                          testingfield.roles_to_assign)
         # add a principal, test that local_roles are adapted
         self.failIf('Reviewers' in testingobj.__ac_local_roles__.keys())
         # the value is now ('Administrators', 'Reviewers')
-        datamanager.set(('Administrators', 'Reviewers'))
+        testingobj.testingField = ['Administrators', 'Reviewers']
         self.failUnless('Reviewers' in testingobj.__ac_local_roles__.keys())
-        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']), testingfield.roles_to_assign)
+        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']),
+                          testingfield.roles_to_assign)
         # remove a group, check managed roles
         # removing a group is made by passing new value where an existing group is no more present
         self.failUnless('Administrators' in testingobj.__ac_local_roles__.keys())
-        datamanager.set(('Reviewers', ))
+        testingobj.testingField = ['Reviewers', ]
         self.failIf('Administrators' in testingobj.__ac_local_roles__.keys())
         # if an external manipulation added a local_role not managed by the field, it is kept
         # add a local_role for 'Reviewers' not managed by our field
-        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']), testingfield.roles_to_assign)
-        testingobj.manage_addLocalRoles('Reviewers', ('Reader', ))
-        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']), testingfield.roles_to_assign + ('Reader', ))
+        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']),
+                          testingfield.roles_to_assign)
+        testingobj.manage_addLocalRoles('Reviewers', ('Contributor', ))
+        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']),
+                          testingfield.roles_to_assign + ('Contributor', ))
         # remove the 'Reviewers' local_roles
-        datamanager.set(())
+        testingobj.testingField = []
         # not managed local_roles are kepts
-        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']), ('Reader', ))
+        self.assertEquals(tuple(testingobj.__ac_local_roles__['Reviewers']), ('Contributor', ))
         # add a not existing principal value, test that it is not set
-        datamanager.set(('toto', ))
+        testingobj.testingField = ['toto', ]
         self.failIf('toto' in testingobj.__ac_local_roles__.keys())
