1e2a8e9f55958d670ae817a476e4a1053f09edb1
Martin Peeters <martin.peeters@affinitic.be>
2014-01-22 09:34:18 +0100

    Restore the LocalRolesToPrincipalsDataManager class to avoid errors.

--- src/collective/z3cform/rolefield/localrolefield.py
+++ src/collective/z3cform/rolefield/localrolefield.py
@@ -94,6 +94,50 @@ def set_local_role_on_object(context, field, event):
     added_principals = set(new_value).difference(set(old_value))
     add_local_roles_to_principals(context, added_principals, roles_to_assign)
 
+
+class LocalRolesToPrincipalsDataManager(AttributeField):
+    """A data manager which set local roles when saving the field."""
+    adapts(Interface, ILocalRolesToPrincipals)
+
+    def set(self, value):
+        """See z3c.form.interfaces.IDataManager"""
+        # set local roles before setting the value so we still have access to the old value
+        roles_to_assign = self.field.roles_to_assign
+        principal_ids = self.context.acl_users.getUserIds() + self.context.acl_users.getGroupIds()
+        # ---1 --- first find assigned roles to remove
+        # it is not that easy to remove local roles because no helper method exists for removing
+        # some specific local roles, only a method for removing every local roles for a list of principals...
+        old_value = self.field.get(self.context) or ()
+        # now check between old_value and value (new value) what is missing
+        removed_principals = set(old_value).difference(set(value))
+        # remove local_roles for removed_principals
+        for local_role in self.context.get_local_roles():
+            # a local_role is like ('Administrators', (u'Contributor', u'Reviewer'))
+            principal = local_role[0]
+            if principal in removed_principals:
+                cleaned_local_roles = list(local_role[1])
+                for role_to_assign in roles_to_assign:
+                    try:
+                        cleaned_local_roles.remove(role_to_assign)
+                    except ValueError:
+                        # if a role to remove was already removed (???) pass
+                        logger.warn("Failed to remove role '%s' for principal '%s' on object '%s'" \
+                                    % (role_to_assign, principal, '/'.join(self.context.getPhysicalPath())))
+                # if there are still some local_roles, use manage_setLocalRoles
+                if cleaned_local_roles:
+                    self.context.manage_setLocalRoles(principal, cleaned_local_roles)
+                else:
+                    # either use manage_delLocalRoles
+                    self.context.manage_delLocalRoles((principal,))
+        # ---2 --- now add new local roles
+        added_principals = set(value).difference(set(old_value))
+        for added_principal in added_principals:
+            if not added_principal in principal_ids:
+                continue
+            self.context.manage_addLocalRoles(added_principal, roles_to_assign)
+        # finally set the value
+        super(LocalRolesToPrincipalsDataManager, self).set(value)   add_local_roles_to_principals(context, added_principals, roles_to_assign)
+
 import plone.supermodel.exportimport
 
 LocalRolesToPrincipalsHandler = plone.supermodel.exportimport.BaseHandler(LocalRolesToPrincipals)
