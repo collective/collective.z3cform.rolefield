8e596019f6e19608bc2c0a2b3e48ab7452d94273
Jean-François Roche <jfroche@affinitic.be>
2014-01-23 18:24:08 +0100

    move local roles function in utils

--- src/collective/z3cform/rolefield/localrolefield.py
+++ src/collective/z3cform/rolefield/localrolefield.py
@@ -2,16 +2,12 @@
 from zope.component.hooks import getSite
 from zope.interface import implementer
 
-from zope.schema import List, NO_VALUE
+from zope.schema import List
 from zope.schema.fieldproperty import FieldPropertyStoredThroughField
 from zope.interface import Invalid
 
-from Products.CMFCore.utils import getToolByName
-
 from .interfaces import ILocalRolesToPrincipals
-
-import logging
-logger = logging.getLogger('collective.z3cform.rolefield')
+from .utils import reset_local_role_on_object
 
 
 @implementer(ILocalRolesToPrincipals)
@@ -44,47 +40,11 @@ class LocalRolesToPrincipals(List):
                               "Contact system administrator!' % role_to_assign)
 
 
-def remove_local_roles_from_principals(context, principals, roles):
-    for local_principal, local_roles in dict(context.get_local_roles()).items():
-        # a local_role is like (u'Contributor', u'Reviewer'))
-        if local_principal in principals:
-            cleaned_local_roles = list(local_roles)
-            for role_to_assign in roles:
-                try:
-                    cleaned_local_roles.remove(role_to_assign)
-                except ValueError:
-                    # if a role to remove was already removed (???) pass
-                    logger.warn("Failed to remove role '%s' for principal '%s' on object '%s'"
-                                % (role_to_assign, local_principal, '/'.join(context.getPhysicalPath())))
-            # if there are still some local_roles, use manage_setLocalRoles
-            if cleaned_local_roles:
-                context.manage_setLocalRoles(local_principal, cleaned_local_roles)
-            else:
-                # either use manage_delLocalRoles
-                context.manage_delLocalRoles((local_principal, ))
-
-
-def add_local_roles_to_principals(context, principals, roles):
-    portal = getSite()
-    acl_users = getToolByName(portal, 'acl_users')
-    principal_ids = acl_users.getUserIds() + acl_users.getGroupIds()
-    for added_principal in principals:
-        if not added_principal in principal_ids:
-            continue
-        context.manage_addLocalRoles(added_principal, roles)
-
-
 def set_local_role_on_object(context, field, event):
     roles_to_assign = field.roles_to_assign
     new_value = event.new_value
     old_value = event.old_value
-    if old_value is NO_VALUE or old_value is None:
-        old_value = []
-    remove_local_roles_from_principals(context,
-                                       old_value,
-                                       roles_to_assign)
-    add_local_roles_to_principals(context, new_value, roles_to_assign)
-
+    reset_local_role_on_object(context, roles_to_assign, old_value, new_value)
 
 import plone.supermodel.exportimport
 
--- src/collective/z3cform/rolefield/utils.py
+++ src/collective/z3cform/rolefield/utils.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from zope.component.hooks import getSite
 from zope.component import getUtility
+from zope.schema import NO_VALUE
+
+from Products.CMFCore.utils import getToolByName
 from plone.dexterity.interfaces import IDexterityFTI
+
+from . import logger
 
 
 def get_field_from_schema(item, fieldInterface):
@@ -12,3 +18,51 @@ def get_field_from_schema(item, fieldInterface):
     for name, field in item_schema.namesAndDescriptions():
         if fieldInterface.providedBy(field):
             yield field
+
+
+def get_suffixed_principals(base_principal_names, suffix):
+    portal = getSite()
+    acl_users = getToolByName(portal, 'acl_users')
+    principal_ids = acl_users.getUserIds() + acl_users.getGroupIds()
+    for principal in principal_ids:
+        for base_principal_name in base_principal_names:
+            if base_principal_name in principal and suffix in principal:
+                yield principal
+
+
+def reset_local_role_on_object(context, roles_to_assign, old_value, new_value):
+    if old_value is not NO_VALUE and old_value is not None:
+        remove_local_roles_from_principals(context,
+                                           old_value,
+                                           roles_to_assign)
+    add_local_roles_to_principals(context, new_value, roles_to_assign)
+
+
+def remove_local_roles_from_principals(context, principals, roles):
+    for local_principal, local_roles in dict(context.get_local_roles()).items():
+        # a local_role is like (u'Contributor', u'Reviewer'))
+        if local_principal in principals:
+            cleaned_local_roles = list(local_roles)
+            for role_to_assign in roles:
+                try:
+                    cleaned_local_roles.remove(role_to_assign)
+                except ValueError:
+                    # if a role to remove was already removed (???) pass
+                    logger.warn("Failed to remove role '%s' for principal '%s' on object '%s'"
+                                % (role_to_assign, local_principal, '/'.join(context.getPhysicalPath())))
+            # if there are still some local_roles, use manage_setLocalRoles
+            if cleaned_local_roles:
+                context.manage_setLocalRoles(local_principal, cleaned_local_roles)
+            else:
+                # either use manage_delLocalRoles
+                context.manage_delLocalRoles((local_principal, ))
+
+
+def add_local_roles_to_principals(context, principals, roles):
+    portal = getSite()
+    acl_users = getToolByName(portal, 'acl_users')
+    principal_ids = acl_users.getUserIds() + acl_users.getGroupIds()
+    for added_principal in principals:
+        if not added_principal in principal_ids:
+            continue
+        context.manage_addLocalRoles(added_principal, roles)
