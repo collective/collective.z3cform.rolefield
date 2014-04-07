b910f5b08e031a45aa1afd7ff1733a6d0aeb58ef
Martin Peeters <martin.peeters@affinitic.be>
2014-04-07 10:29:41 +0200

    Add the message factory. refs #7262

--- src/collective/z3cform/rolefield/__init__.py
+++ src/collective/z3cform/rolefield/__init__.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 import logging
+from zope.i18nmessageid import MessageFactory
+
+
 logger = logging.getLogger('collective.z3cform.rolefield')
 
 from .localrolefield import LocalRolesToPrincipals
 
 #pep8
 LocalRolesToPrincipals
+
+_ = MessageFactory('collective.z3cform.rolefield')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
